e0967734e86cc9e11617a25fb1ac787ec6845a98
Tomas <frcrewlapd@gmail.com>
2023-08-24 17:17:28 -0300

    Fix Header Desc

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -7,14 +7,14 @@ const Header = () => {
       DESPUES, HACER UN MAP PARA RECORRER EL JSON Y CREAR UN HEADERCARD POR CADA OBJETO DEL JSON.
   */
   return (
-    <div className='flex justify-between pt-5 px-40'>
-      <HeaderCard title="ELEGI UN AUTO" img='src\assets\finger.webp' desc="Acorde a tus intereses"></HeaderCard>
+    <div className='grid grid-cols-4 grid-rows-0 gap-0 pt-5 px-40'>
+      <HeaderCard title="ELEGI UN AUTO" img='src\assets\finger.webp' desc="Acorde a tus intereses."></HeaderCard>
       
-      <HeaderCard title="RETIRA EL AUTO" img='src\assets\cred.webp' desc="Combina un lugar de encuentro con el propietario"></HeaderCard>
+      <HeaderCard title="RETIRA EL AUTO" img='src\assets\cred.webp' desc="Combina un lugar de encuentro con el propietario."></HeaderCard>
       
-      <HeaderCard title="VIAJA SEGURO" img='src\assets\car.webp' desc="Cocoche cuenta con un seguro todo riesgo y retiene un deposito en la tarjeta"></HeaderCard>
+      <HeaderCard title="VIAJA SEGURO" img='src\assets\car.webp' desc="Cocoche cuenta con un seguro todo riesgo y retiene un deposito en la tarjeta como garantía."></HeaderCard>
 
-      <HeaderCard title="REALIZA EL PAGO" img='src\assets\card.webp' desc="Abonas el costo del alquiler"></HeaderCard>
+      <HeaderCard title="REALIZA EL PAGO" img='src\assets\card.webp' desc="Abonas el costo del alquiler."></HeaderCard>
 
     </div>
   )
--- src/components/header/headerCard.jsx
+++ src/components/header/headerCard.jsx
@@ -3,9 +3,9 @@ const HeaderCard = (props) => {
     <span className='font-nunito'>
 
       <div class="max-w-sm rounded overflow-hidden justify-center items-center flex flex-col">
-        <img class="w-[128px] h-[128px]" src={props.img} alt={props.title}></img>
+        <img class="w-[100px] h-[100px]" src={props.img} alt={props.title}></img>
         <div class="px-6 py-4 flex flex-col items-center justify-center">
-          <h2 class="font-black text-gray-700/80 text-xl mb-2">{props.title}</h2>
+          <h2 class="font-black text-gray-700/80 text-xl mb-2 mt-3">{props.title}</h2>
           <p class="text-gray-600 text-center opacity-80">
             {props.desc}
           </p>
